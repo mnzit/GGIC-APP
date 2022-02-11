4abe465c41816a56c100414926a064df26f0791f
mnzit <mnzitshakya@gmail.com>
2022-02-12 00:38:03 +0545

    payara jar download plugin added

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 mvn clean package
-java -jar app-server.jar --deploy target/*.war --nocluster --port 9091 #payara
+java -jar target/dependency/app-server.jar --deploy target/*.war --nocluster --port 9091 #payara
 #java -jar target/dependency/app-server.jar --port 9091 target/*.war #tomcat lite
--- stop.sh
+++ stop.sh
@@ -1,2 +1,2 @@
-lsof -i:8080
+lsof -i:9091
 kill -9 $(lsof -t -i:9091)
