06f07acea8aeb2ea5797e15de479885f3dd60cf0
mnzit <mnzitshakya@gmail.com>
2022-02-13 16:33:22 +0545

    payara micro added as a application server

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 mvn clean package
-java -jar target/dependency/app-server.jar --port 9091 target/*.war #tomcat lite
+#java -jar target/dependency/app-server.jar --port 9091 target/*.war #tomcat lite
+
+java -jar target/dependency/app-server.jar --port 9091 --deploy target/*.war --nocluster
 
